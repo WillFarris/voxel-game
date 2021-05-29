f1956627e25b91c13ca41eedd3d7064fb31dc497
Will Farris <9190155+WillFarris@users.noreply.github.com>
2021-05-29 00:05:21 -0700

    use directional light on gles

--- shaders/cube_fragment_es.glsl
+++ shaders/cube_fragment_es.glsl
@@ -6,23 +6,20 @@ in vec3 v_normal;
 in vec3 v_position;
 in vec2 v_tex_coords;
 
-
+uniform vec3 sunlight_direction;
 uniform sampler2D texture_map;
 
 out vec4 color;
 
 void main() {
-
-    vec3 light = vec3(0.3, 0.7, -0.701);
-
-    float diffuse = max(dot(normalize(v_normal), normalize(light)), 0.2);
+    float diffuse = max(dot(normalize(v_normal), normalize(sunlight_direction)), 0.2);
 
     vec3 tex_color = texture(texture_map, v_tex_coords).rgb;
 
-    /*vec3 camera_dir = normalize(-v_position);
-    vec3 half_direction = normalize(normalize(light) + camera_dir);
+    vec3 camera_dir = normalize(-v_position);
+    vec3 half_direction = normalize(normalize(sunlight_direction) + camera_dir);
     float specular = pow(max(dot(half_direction, normalize(v_normal)), 0.0), 16.0);
-    tex_color = tex_color + specular * vec3(0.9);*/
+    tex_color = tex_color + specular * vec3(0.9);
 
     color = vec4(vec3(0.01) + diffuse * tex_color, 1.0);
 }

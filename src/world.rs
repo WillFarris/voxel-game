7e7ec887da5d1b2d82559ad2d1dc7d16a289ab25
Will Farris <9190155+GetLiquid@users.noreply.github.com>
2021-06-17 23:18:21 -0700

    fix foliage generation

--- src/world.rs
+++ src/world.rs
@@ -115,23 +115,62 @@ impl<'a> World<'a> {
         for block_x in 0..CHUNK_SIZE {
             for block_y in 0..CHUNK_SIZE {
                 for block_z in 0..CHUNK_SIZE {
-                    let global_x = (block_x as isize + (chunk_index.x * CHUNK_SIZE as isize)) as f64;
-                    let global_y = (block_y as isize + (chunk_index.y * CHUNK_SIZE as isize)) as f64;
-                    let global_z = (block_z as isize + (chunk_index.z * CHUNK_SIZE as isize)) as f64;
-                    let surface_y = self.surface_noise(global_x, global_z);
-                    if global_y < surface_y {
-                        if global_y == surface_y.floor() {
+                    let global_x = block_x as isize + (chunk_index.x * CHUNK_SIZE as isize);
+                    let global_y = block_y as isize + (chunk_index.y * CHUNK_SIZE as isize);
+                    let global_z = block_z as isize + (chunk_index.z * CHUNK_SIZE as isize);
+                    let surface_y = self.surface_noise(global_x as f64, global_z as f64);
+                    if (global_y as f64) < surface_y {
+                        if global_y == surface_y.floor() as isize {
                             chunk.blocks[block_x][block_y][block_z] = 2;
-                            if block_y < CHUNK_SIZE-1 {
-                                chunk.blocks[block_x][block_y+1][block_z] = 12;
-                            }
-                        } else if global_y < (7.0 * surface_y/8.0).floor() {
+                            self.place_ground_foliage(global_x, global_y + 1, global_z);
+                        } else if (global_y as f64) < (7.0 * surface_y/8.0).floor() {
                             chunk.blocks[block_x][block_y][block_z] = 1;
                         } else {
                             chunk.blocks[block_x][block_y][block_z] = 3;
                         }
                     }
                 }
+            }
+        }
+
+        for (position, chunk) in &mut self.chunks {
+            if let Some(queue) = self.generation_queue.get(position) {
+                for (block_pos, block_id) in queue {
+                    chunk.blocks[block_pos.x][block_pos.y][block_pos.z] = *block_id;
+                }
+            }
+        }
+    }
+
+    fn place_ground_foliage(&mut self, x: isize, y: isize, z: isize) {
+        match rand::random::<usize>()%100 {
+            50..=99 => {
+                let mut block_id = rand::random::<usize>()%10;
+                if block_id <= 6 { block_id = 12 } else if block_id <= 8 {block_id = 13} else {block_id = 10};
+
+                let (position, block_index) = World::chunk_and_block_index(&Vector3::new(x, y, z));
+                if let Some(chunk) = self.chunks.get_mut(&position) {
+                    chunk.blocks[block_index.x][block_index.y][block_index.z] = block_id;
+                } else {
+                    self.append_queued_block(block_id, &position, &block_index);
+                }
+            }
+            40 => {
+                self.place_tree(Vector3::new(x, y, z))
+            }
+            _ => {
+
+            }
+        }
+    }
+
+    fn append_queued_block(&mut self, block_id: usize, chunk_index: &Vector3<isize>, block_index: &Vector3<usize>) {
+        if let Some(list) = self.generation_queue.get_mut(chunk_index) {
+            list.push_back((*block_index, block_id));
+        } else {
+            self.generation_queue.insert(*chunk_index, LinkedList::new());
+            if let Some(list) = self.generation_queue.get_mut(chunk_index) {
+                list.push_back((*block_index, block_id));
             }
         }
     }
@@ -158,7 +197,36 @@ impl<'a> World<'a> {
     }
 
 
-    pub fn place_tree(&mut self, index: Vector3<isize>) {
+    pub fn place_tree(&mut self, world_pos: Vector3<isize>) {
+
+        
+
+        for y in 0..5 {
+            //chunk.blocks[block_index.x][block_index.y+y][block_index.z] = 9;
+            let (chunk_index, block_index) = World::chunk_and_block_index(&(world_pos + Vector3::new(0, y, 0)));
+            if let Some(chunk) = self.chunks.get_mut(&chunk_index) {
+                chunk.blocks[block_index.x][block_index.y][block_index.z] = 9;
+            } else {
+                self.append_queued_block(9, &chunk_index, &block_index);
+            }
+        }
+
+        for x in -1..=1 {
+            for z in -1..=1 {
+                for y in 3..=5 {
+                    if (x == -1 && z == -1 && y == 5) || (x == 1 && z == 1 && y == 5) || (x == -1 && z == 1 && y == 5) || (x == 1 && z == -1 && y == 5) {
+                        continue;
+                    }
+                    let (chunk_index, block_index) = World::chunk_and_block_index(&(world_pos + Vector3::new(x, y, z)));
+                    if let Some(chunk) = self.chunks.get_mut(&chunk_index) {
+                        chunk.blocks[block_index.x][block_index.y][block_index.z] = 11;
+                    } else {
+                        self.append_queued_block(11, &chunk_index, &block_index);
+                    }
+                    //chunk.blocks[x][y][z] = 11;
+                }
+            }
+        }
 
         //
 
@@ -382,14 +450,14 @@ impl<'a> World<'a> {
     pub fn block_at_global_pos(&self, world_pos: Vector3<isize>) -> usize {
         let (chunk_index, block_index) = World::chunk_and_block_index(&world_pos);
         if let Some(chunk) = self.chunks.get(&chunk_index) {
-            return chunk.block_at_chunk_pos(&block_index);
+            chunk.block_at_chunk_pos(&block_index)
         } else {
             0
         }
     }
 
     pub fn collision_at_world_pos(&self, world_pos: Vector3<isize>) -> bool {
-        self.block_at_global_pos(world_pos) != 0
+        0 != self.block_at_global_pos(world_pos)
     }
 
     pub fn gen_chunk_mesh(&mut self, chunk_index: &Vector3<isize>) {
